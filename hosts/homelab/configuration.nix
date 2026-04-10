abc0d7547661f41b804c5dc509fcc55afe434bd0
zerroxxx9 <190294721+zerroxxx9@users.noreply.github.com>
2026-04-10 16:27:32 +0200

    removed module homelab

--- hosts/homelab/configuration.nix
+++ hosts/homelab/configuration.nix
@@ -1,35 +1,22 @@
 { hostVariables, pkgs, lib, ... }:
-
 {
   imports = [
-    ../software/tailscale.nix
     ./hardware-configuration.nix
-    ];
-
-  homelab = {
-    enable = true;
-    defaultUser = hostVariables.username;
-    startMenuLaunchers = true;
-  };
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   networking = {
     hostName = hostVariables.host;
     useHostResolvConf = lib.mkForce false;
   };
-
   services.resolved.enable = true;
-
   users.users.${hostVariables.username} = {
     isNormalUser = true;
     description = hostVariables.username;
     extraGroups = ["networkmanager" "wheel"];
   };
-
   programs.direnv.enable = true;
-
   environment.systemPackages = with pkgs; [
     wget
     curl
@@ -39,11 +26,8 @@
     zip
     #lab specific
     opencloud
-    tailscale
   ];
-
   time.timeZone = "Europe/Berlin";
-
   i18n.defaultLocale = lib.mkForce "de_DE.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS        = "de_DE.UTF-8";
@@ -56,16 +40,6 @@
     LC_TELEPHONE      = "de_DE.UTF-8";
     LC_TIME           = "de_DE.UTF-8";
   };
-
-  tailscale = {
-    enable = true;
-    exitNode = true;
-    subnetRoutes = [ "192.168.1.0/24" ];
-    authKeyFile = config.age.secrets.tailscale.path;
-    useSSH = true;
-  };
-
   nixpkgs.config.allowUnfree = true;
-
   system.stateVersion = hostVariables.stateVersion;
 }

7320e02e0c09424273d463aed15f911d08715fb5
zerroxxx9 <190294721+zerroxxx9@users.noreply.github.com>
2026-04-20 17:24:21 +0200

    fix paperless config

--- modules/software/paperless-ngx.nix
+++ modules/software/paperless-ngx.nix
@@ -5,23 +5,22 @@
   };
 
   config = lib.mkIf config.modules.software.paperless-ngx.enable {
-    services.paperless-ngx = {
+    services.paperless = {
       enable = true;
       address = "127.0.0.1";
       port = 1337;
-      url = "https://homelab.tail11bba0.ts.net";
     };
 
     systemd.services.tailscale-serve-paperless-ngx = {
       description = "Publish Paperless via Tailscale Serve";
-      after = [ "network-online.target" "tailscaled.service" "paperless-ngx.service" ];
-      wants = [ "network-online.target" "tailscaled.service" "paperless-ngx.service" ];
+      after = [ "network-online.target" "tailscaled.service" "paperless-web.service" ];
+      wants = [ "network-online.target" "tailscaled.service" "paperless-web.service" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = true;
-        ExecStart = "${config.services.paperless-ngx.package}/bin/tailscale serve --bg --yes https+insecure://127.0.0.1:1337";
-        ExecStop = "${config.services.paperless-ngx.package}/bin/tailscale serve --https=443 off";
+        ExecStart = "${config.services.tailscale.package}/bin/tailscale serve --bg --yes https+insecure://127.0.0.1:1337";
+        ExecStop = "${config.services.tailscale.package}/bin/tailscale serve --https=443 off";
       };
     };
   };

6595ba40baffe18126d1ea913f7a39a433e9c8e1
zerroxxx9 <190294721+zerroxxx9@users.noreply.github.com>
2026-07-01 22:15:36 +0200

    neovim config

--- modules/software/neovim.nix
+++ modules/software/neovim.nix
@@ -42,8 +42,16 @@ in {
         extraPackages = cfg.extraPackages;
 
         plugins = with pkgs.vimPlugins; [
+          catppuccin-nvim
+          bufferline-nvim
+          gitsigns-nvim
+          indent-blankline-nvim
+          lualine-nvim
+          nvim-treesitter.withAllGrammars
+          nvim-web-devicons
           vim-sensible
           vim-nix
+          which-key-nvim
         ];
 
         extraLuaConfig = ''
@@ -66,8 +74,59 @@ in {
           vim.opt.undofile = true
           vim.opt.updatetime = 250
 
+          require("catppuccin").setup({
+            flavour = "mocha",
+            transparent_background = false,
+            integrations = {
+              bufferline = true,
+              gitsigns = true,
+              indent_blankline = {
+                enabled = true,
+              },
+              native_lsp = {
+                enabled = true,
+              },
+              treesitter = true,
+              which_key = true,
+            },
+          })
+          vim.cmd.colorscheme("catppuccin")
+
+          require("nvim-treesitter.configs").setup({
+            highlight = {
+              enable = true,
+            },
+            indent = {
+              enable = true,
+            },
+          })
+
+          require("gitsigns").setup()
+          require("ibl").setup()
+          require("which-key").setup()
+
+          require("lualine").setup({
+            options = {
+              component_separators = "",
+              globalstatus = true,
+              section_separators = "",
+              theme = "catppuccin",
+            },
+          })
+
+          require("bufferline").setup({
+            options = {
+              diagnostics = "nvim_lsp",
+              separator_style = "thin",
+              show_buffer_close_icons = false,
+              show_close_icon = false,
+            },
+          })
+
           vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Write buffer" })
           vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", { desc = "Quit window" })
+          vim.keymap.set("n", "<tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+          vim.keymap.set("n", "<s-tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous buffer" })
 
           ${cfg.extraLuaConfig}
         '';

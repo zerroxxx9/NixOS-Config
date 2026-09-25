3e6b9bd5e1049ac4ba5e43b837dd55c6e5a92b59
zerroxxx9 <190294721+zerroxxx9@users.noreply.github.com>
2026-09-25 21:10:26 +0200

    kvm-intel

--- hosts/homelab/configuration.nix
+++ hosts/homelab/configuration.nix
@@ -11,6 +11,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = [ "kvm-intel" ];
   networking = {
     hostName = hostVariables.host;
     useHostResolvConf = lib.mkForce false;
